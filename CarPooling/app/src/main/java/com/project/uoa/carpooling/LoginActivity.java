242dd589b8d25261f1c8aea8df55da12cf5b20fc
IcyCaster <chester.booker.nz@gmail.com>
2016-03-17 16:02:32 +1300

    Basic login page UI - now trying to remove header bar

--- CarPooling/app/src/main/java/com/project/uoa/carpooling/LoginActivity.java
+++ CarPooling/app/src/main/java/com/project/uoa/carpooling/LoginActivity.java
@@ -2,12 +2,30 @@ package com.project.uoa.carpooling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
+        // Removes title bar on the login screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        //TODO: Currently there is no MainActivity present
+        moveTaskToBack(true);
     }
 }
