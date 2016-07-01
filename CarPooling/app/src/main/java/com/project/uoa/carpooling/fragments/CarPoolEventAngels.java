7233f308ea41afdc22272ab8a2a73d145031812d
Angel <a.tuyay.castro@gmail.com>
2016-07-01 16:32:23 +1200

    Added Firebase Functionality to Settings Fragment

--- CarPooling/app/src/main/java/com/project/uoa/carpooling/fragments/CarPoolEventAngels.java
+++ CarPooling/app/src/main/java/com/project/uoa/carpooling/fragments/CarPoolEventAngels.java
@@ -4,11 +4,24 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.project.uoa.carpooling.R;
+import com.project.uoa.carpooling.firebaseModels.DBItemModel;
 
 
 /**
@@ -20,6 +33,19 @@ import com.project.uoa.carpooling.R;
  * create an instance of this fragment.
  */
 public class CarPoolEventAngels extends Fragment {
+
+    //TODO Will move later.
+    public static class MessageViewHolder extends RecyclerView.ViewHolder {
+        public TextView messageTextView;
+        public TextView messengerTextView;
+
+        public MessageViewHolder(View v) {
+            super(v);
+            messageTextView = (TextView) itemView.findViewById(R.id.messageTextView);
+            messengerTextView = (TextView) itemView.findViewById(R.id.messengerTextView);
+        }
+    }
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,6 +56,18 @@ public class CarPoolEventAngels extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    //UI Components
+    private Button mSendButton;
+    private RecyclerView mMessageRecyclerView;
+    private LinearLayoutManager mLinearLayoutManager;
+    private ProgressBar mProgressBar;
+    private EditText mMessageEditText;
+
+    // Firebase Instance Variables
+    private DatabaseReference mFirebaseDatabaseReference;
+    private FirebaseRecyclerAdapter<DBItemModel, MessageViewHolder>
+            mFirebaseAdapter;
 
     public CarPoolEventAngels() {
         // Required empty public constructor
@@ -65,8 +103,91 @@ public class CarPoolEventAngels extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_car_pool_event_angels, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_car_pool_event_angels, container, false);
+
+        // Initialize ProgressBar and RecyclerView.
+        mProgressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+        mMessageRecyclerView = (RecyclerView) rootView.findViewById(R.id.messageRecyclerView);
+        mLinearLayoutManager = new LinearLayoutManager(getActivity());
+        mLinearLayoutManager.setStackFromEnd(true);
+        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+
+        // New child entries
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<DBItemModel,
+                MessageViewHolder>(
+                DBItemModel.class,
+                R.layout.item_message,
+                MessageViewHolder.class,
+                mFirebaseDatabaseReference.child("messages")) {
+
+            @Override
+            protected void populateViewHolder(MessageViewHolder viewHolder,
+                                              DBItemModel DBItemModel, int position) {
+                mProgressBar.setVisibility(ProgressBar.INVISIBLE);
+                viewHolder.messageTextView.setText(DBItemModel.getText());
+                viewHolder.messengerTextView.setText(DBItemModel.getName());
+            }
+        };
+
+        mFirebaseAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+
+                // All below is kind of optional.
+                int friendlyMessageCount = mFirebaseAdapter.getItemCount();
+                int lastVisiblePosition =
+                        mLinearLayoutManager.findLastCompletelyVisibleItemPosition();
+                // If the recycler view is initially being loaded or the
+                // user is at the bottom of the list, scroll to the bottom
+                // of the list to show the newly added message.
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (friendlyMessageCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    mMessageRecyclerView.scrollToPosition(positionStart);
+                }
+            }
+        });
+
+        mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mMessageRecyclerView.setAdapter(mFirebaseAdapter);
+
+        mMessageEditText = (EditText) rootView.findViewById(R.id.messageEditText);
+        mMessageEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence.toString().trim().length() > 0) {
+                    mSendButton.setEnabled(true);
+                } else {
+                    mSendButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+
+        mSendButton = (Button) rootView.findViewById(R.id.sendButton);
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBItemModel DBItemModel = new
+                        DBItemModel(mMessageEditText.getText().toString(),
+                        "Anonymous");
+                mFirebaseDatabaseReference.child("messages")
+                        .push().setValue(DBItemModel);
+                mMessageEditText.setText("");
+            }
+        });
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
